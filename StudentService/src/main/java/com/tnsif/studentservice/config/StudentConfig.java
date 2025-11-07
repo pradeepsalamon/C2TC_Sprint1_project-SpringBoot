37775694cc8e9517ac29e037a3e22b7196c9c0db
pradeepsalamon <pradeepsalamon14@gmail.com>
2025-11-07 21:47:32 +0530

    config file updated

--- StudentService/src/main/java/com/tnsif/studentservice/config/StudentConfig.java
+++ StudentService/src/main/java/com/tnsif/studentservice/config/StudentConfig.java
@@ -1,4 +1,4 @@
-package com.tnif.studentservice.config;
+package com.tnsif.studentservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
